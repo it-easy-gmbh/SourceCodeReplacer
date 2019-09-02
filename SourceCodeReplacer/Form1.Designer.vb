69f7272918967a05df22dc115d623425609e9702
Lars Kempa <kempa@it-easy.de>
2019-09-02 15:29:59 +0200

    Projektdateien hinzufügen.

---
+++ SourceCodeReplacer/Form1.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonRun = New System.Windows.Forms.Button()
+        Me.TextBoxErgebnis = New System.Windows.Forms.TextBox()
+        Me.TextBoxPfad = New System.Windows.Forms.TextBox()
+        Me.LabelBasisPfad = New System.Windows.Forms.Label()
+        Me.ComboBoxErweiterung = New System.Windows.Forms.ComboBox()
+        Me.LabelErweiterung = New System.Windows.Forms.Label()
+        Me.ButtonBrowse = New System.Windows.Forms.Button()
+        Me.TextBoxErsetzen = New System.Windows.Forms.TextBox()
+        Me.TextBoxSuchen = New System.Windows.Forms.TextBox()
+        Me.LabelSuche = New System.Windows.Forms.Label()
+        Me.LabelErsetzen = New System.Windows.Forms.Label()
+        Me.LabelTreffer = New System.Windows.Forms.Label()
+        Me.LabelCount = New System.Windows.Forms.Label()
+        Me.ButtonDryRun = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ButtonRun
+        '
+        Me.ButtonRun.Location = New System.Drawing.Point(713, 113)
+        Me.ButtonRun.Name = "ButtonRun"
+        Me.ButtonRun.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonRun.TabIndex = 0
+        Me.ButtonRun.Text = "Run"
+        Me.ButtonRun.UseVisualStyleBackColor = True
+        '
+        'TextBoxErgebnis
+        '
+        Me.TextBoxErgebnis.Location = New System.Drawing.Point(12, 142)
+        Me.TextBoxErgebnis.Multiline = True
+        Me.TextBoxErgebnis.Name = "TextBoxErgebnis"
+        Me.TextBoxErgebnis.ReadOnly = True
+        Me.TextBoxErgebnis.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBoxErgebnis.Size = New System.Drawing.Size(776, 269)
+        Me.TextBoxErgebnis.TabIndex = 1
+        '
+        'TextBoxPfad
+        '
+        Me.TextBoxPfad.Location = New System.Drawing.Point(147, 37)
+        Me.TextBoxPfad.Name = "TextBoxPfad"
+        Me.TextBoxPfad.Size = New System.Drawing.Size(396, 20)
+        Me.TextBoxPfad.TabIndex = 1
+        '
+        'LabelBasisPfad
+        '
+        Me.LabelBasisPfad.AutoSize = True
+        Me.LabelBasisPfad.Location = New System.Drawing.Point(23, 40)
+        Me.LabelBasisPfad.Name = "LabelBasisPfad"
+        Me.LabelBasisPfad.Size = New System.Drawing.Size(57, 13)
+        Me.LabelBasisPfad.TabIndex = 2
+        Me.LabelBasisPfad.Text = "Basis-Pfad"
+        '
+        'ComboBoxErweiterung
+        '
+        Me.ComboBoxErweiterung.FormattingEnabled = True
+        Me.ComboBoxErweiterung.Items.AddRange(New Object() {"VB", "CS", "HTM", "HTML", "CSS", "XML"})
+        Me.ComboBoxErweiterung.Location = New System.Drawing.Point(147, 63)
+        Me.ComboBoxErweiterung.Name = "ComboBoxErweiterung"
+        Me.ComboBoxErweiterung.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBoxErweiterung.TabIndex = 3
+        '
+        'LabelErweiterung
+        '
+        Me.LabelErweiterung.AutoSize = True
+        Me.LabelErweiterung.Location = New System.Drawing.Point(23, 66)
+        Me.LabelErweiterung.Name = "LabelErweiterung"
+        Me.LabelErweiterung.Size = New System.Drawing.Size(63, 13)
+        Me.LabelErweiterung.TabIndex = 2
+        Me.LabelErweiterung.Text = "Erweiterung"
+        '
+        'ButtonBrowse
+        '
+        Me.ButtonBrowse.Location = New System.Drawing.Point(549, 35)
+        Me.ButtonBrowse.Name = "ButtonBrowse"
+        Me.ButtonBrowse.Size = New System.Drawing.Size(26, 23)
+        Me.ButtonBrowse.TabIndex = 0
+        Me.ButtonBrowse.Text = "..."
+        Me.ButtonBrowse.UseVisualStyleBackColor = True
+        '
+        'TextBoxErsetzen
+        '
+        Me.TextBoxErsetzen.Location = New System.Drawing.Point(147, 116)
+        Me.TextBoxErsetzen.Name = "TextBoxErsetzen"
+        Me.TextBoxErsetzen.Size = New System.Drawing.Size(396, 20)
+        Me.TextBoxErsetzen.TabIndex = 4
+        '
+        'TextBoxSuchen
+        '
+        Me.TextBoxSuchen.Location = New System.Drawing.Point(147, 90)
+        Me.TextBoxSuchen.Name = "TextBoxSuchen"
+        Me.TextBoxSuchen.Size = New System.Drawing.Size(396, 20)
+        Me.TextBoxSuchen.TabIndex = 4
+        '
+        'LabelSuche
+        '
+        Me.LabelSuche.AutoSize = True
+        Me.LabelSuche.Location = New System.Drawing.Point(23, 93)
+        Me.LabelSuche.Name = "LabelSuche"
+        Me.LabelSuche.Size = New System.Drawing.Size(74, 13)
+        Me.LabelSuche.TabIndex = 2
+        Me.LabelSuche.Text = "Suche nach..."
+        '
+        'LabelErsetzen
+        '
+        Me.LabelErsetzen.AutoSize = True
+        Me.LabelErsetzen.Location = New System.Drawing.Point(23, 119)
+        Me.LabelErsetzen.Name = "LabelErsetzen"
+        Me.LabelErsetzen.Size = New System.Drawing.Size(87, 13)
+        Me.LabelErsetzen.TabIndex = 2
+        Me.LabelErsetzen.Text = "Ersetzen durch..."
+        '
+        'LabelTreffer
+        '
+        Me.LabelTreffer.Location = New System.Drawing.Point(12, 428)
+        Me.LabelTreffer.Name = "LabelTreffer"
+        Me.LabelTreffer.Size = New System.Drawing.Size(44, 13)
+        Me.LabelTreffer.TabIndex = 2
+        Me.LabelTreffer.Text = "Treffer:"
+        '
+        'LabelCount
+        '
+        Me.LabelCount.Location = New System.Drawing.Point(62, 428)
+        Me.LabelCount.Name = "LabelCount"
+        Me.LabelCount.Size = New System.Drawing.Size(70, 13)
+        Me.LabelCount.TabIndex = 2
+        Me.LabelCount.Text = "100"
+        '
+        'ButtonDryRun
+        '
+        Me.ButtonDryRun.Location = New System.Drawing.Point(713, 84)
+        Me.ButtonDryRun.Name = "ButtonDryRun"
+        Me.ButtonDryRun.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonDryRun.TabIndex = 0
+        Me.ButtonDryRun.Text = "Dry-Run"
+        Me.ButtonDryRun.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBoxSuchen)
+        Me.Controls.Add(Me.TextBoxErsetzen)
+        Me.Controls.Add(Me.ComboBoxErweiterung)
+        Me.Controls.Add(Me.LabelCount)
+        Me.Controls.Add(Me.LabelTreffer)
+        Me.Controls.Add(Me.LabelErsetzen)
+        Me.Controls.Add(Me.LabelSuche)
+        Me.Controls.Add(Me.LabelErweiterung)
+        Me.Controls.Add(Me.LabelBasisPfad)
+        Me.Controls.Add(Me.TextBoxPfad)
+        Me.Controls.Add(Me.ButtonBrowse)
+        Me.Controls.Add(Me.TextBoxErgebnis)
+        Me.Controls.Add(Me.ButtonDryRun)
+        Me.Controls.Add(Me.ButtonRun)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonRun As Button
+    Friend WithEvents TextBoxErgebnis As TextBox
+    Friend WithEvents TextBoxPfad As TextBox
+    Friend WithEvents LabelBasisPfad As Label
+    Friend WithEvents ComboBoxErweiterung As ComboBox
+    Friend WithEvents LabelErweiterung As Label
+    Friend WithEvents ButtonBrowse As Button
+    Friend WithEvents TextBoxErsetzen As TextBox
+    Friend WithEvents TextBoxSuchen As TextBox
+    Friend WithEvents LabelSuche As Label
+    Friend WithEvents LabelErsetzen As Label
+    Friend WithEvents LabelTreffer As Label
+    Friend WithEvents LabelCount As Label
+    Friend WithEvents ButtonDryRun As Button
+End Class
---
+++ SourceCodeReplacer/Form1.vb
@@ -0,0 +1,168 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Private Extension As String
+    Private SucheString As String
+    Private ErsetzenString As String
+    Private BasisPfad As String
+    Private _Count As Integer
+
+    Private Property Count As Integer
+        Set(value As Integer)
+            If value = 0 Then
+                Me.LabelCount.Text = "0"
+                _Count = 0
+            Else
+                _Count += value
+                Me.LabelCount.Invoke(Sub() Me.LabelCount.Text = _Count)
+            End If
+        End Set
+        Get
+            Return _Count
+        End Get
+    End Property
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.LabelCount.Text = 0
+    End Sub
+
+    Private Sub ButtonRun_Click(sender As Object, e As EventArgs) Handles ButtonRun.Click
+        SetzeVariablen()
+        Dim t As New Task(Sub() Ersetzen(False))
+        t.Start()
+    End Sub
+
+    Private Sub ButtonDryRun_Click(sender As Object, e As EventArgs) Handles ButtonDryRun.Click
+        SetzeVariablen()
+        Dim t As New Task(Sub() Ersetzen(True))
+        t.Start()
+    End Sub
+
+    Private Sub SetzeVariablen()
+        Me.SucheString = Me.TextBoxSuchen.Text
+        Me.ErsetzenString = Me.TextBoxErsetzen.Text
+        Me.BasisPfad = Me.TextBoxPfad.Text
+        Me.Extension = Me.ComboBoxErweiterung.Text.ToLower
+        Me.Count = 0
+    End Sub
+
+    Private Sub Ersetzen(DryRun As Boolean)
+        If Not Directory.Exists(Me.BasisPfad) OrElse String.IsNullOrWhiteSpace(Me.Extension) OrElse String.IsNullOrWhiteSpace(Me.SucheString) OrElse String.IsNullOrWhiteSpace(Me.ErsetzenString) Then
+            MsgBox("Alle notwendigen Felder füllen")
+            Exit Sub
+        End If
+        Dim fileLIS As New List(Of FileInfo)
+        AddFilesToList(Me.TextBoxPfad.Text, fileLIS)
+        Dim sb As New StringBuilder
+        For Each datei In fileLIS
+            Try
+                '>>> Prüfen, ob BOM vorhanden ist
+                Dim bytes() As Byte = Nothing
+                Using fsSource As FileStream = New FileStream(datei.FullName, FileMode.Open, FileAccess.Read)
+                    bytes = New Byte(3) {}
+                    Dim numBytesToRead As Integer = 3
+                    Dim numBytesRead As Integer = 0
+                    While (numBytesToRead > 0)
+                        ' Read may return anything from 0 to numBytesToRead.
+                        Dim n As Integer = fsSource.Read(bytes, numBytesRead, numBytesToRead)
+                        ' Break when the end of the file is reached.
+                        If (n = 0) Then
+                            Exit While
+                        End If
+                        numBytesRead = (numBytesRead + n)
+                        numBytesToRead = (numBytesToRead - n)
+                    End While
+                End Using
+                Dim enc = New UTF8Encoding(True)
+                Dim preamble = enc.GetPreamble()
+                If preamble.Where(Function(p, i) p <> bytes(i)).Any() Then
+                    '>>> Nur wenn kein UTF8-BOM vorhanden ist, Datei umwandeln:
+                    Dim tmpCodierungsErgebnis As TextdateiRückgabe = TextdateiMitCodierungUTF8ohneBomOderAnsiKorrektLesen(datei.FullName)
+                    If tmpCodierungsErgebnis.FehlerFlag = False Then
+                        File.WriteAllText(datei.FullName, tmpCodierungsErgebnis.Text, Encoding.UTF8)
+                    End If
+                End If
+                Dim count As Integer
+                Dim content As String = File.ReadAllText(datei.FullName, Encoding.UTF8)
+                count = Regex.Matches(content, Me.SucheString).Count
+                If count > 0 Then
+                    Me.Count = count
+                    If DryRun = False Then
+                        content = content.Replace(Me.SucheString, Me.ErsetzenString)
+                        File.WriteAllText(datei.FullName, content, Encoding.UTF8)
+                    End If
+                    sb.AppendLine($"{datei.Name} ({count} Vorkommen)")
+                    Me.TextBoxErgebnis.Invoke(Sub() Me.TextBoxErgebnis.Text = sb.ToString)
+                End If
+            Catch ex As Exception
+                Stop
+            End Try
+        Next
+    End Sub
+
+    Private Sub AddFilesToList(Pfad As String, FileLIS As List(Of FileInfo))
+        For Each file In Directory.GetFiles(Pfad, "*." & Me.Extension)
+            FileLIS.Add(New FileInfo(file))
+        Next
+        For Each folder In Directory.GetDirectories(Pfad)
+            AddFilesToList(folder, FileLIS)
+        Next
+    End Sub
+
+    Private Sub ButtonBrowse_Click(sender As Object, e As EventArgs) Handles ButtonBrowse.Click
+        Dim ddd As New FolderBrowserDialog
+        If ddd.ShowDialog = DialogResult.OK Then
+            Me.TextBoxPfad.Text = ddd.SelectedPath
+        End If
+    End Sub
+
+
+
+
+
+
+#Region " Private Shared Function: TextdateiMitCodierungUTF8ohneBomOderAnsiKorrektLesen (Textdatei mit Codierung UTF8ohneBOM oder ANSI korrekt einlesen) "
+    ''' <summary>
+    ''' Bei Textdateien ohne BOM-Markierung am Anfang muss anhand des Inhalts entschieden werden, ob es sich um UTF8 oder ANSI handelt
+    ''' </summary>
+    ''' <param name="DateiPfad">Pfad zur Textdatei</param>
+    ''' <returns>Struktur: Korrekt eingelesener Text bei Erfolg, sonst leerer Text, sowie Fehlerflag</returns>
+    ''' <remarks></remarks>
+    Private Shared Function TextdateiMitCodierungUTF8ohneBomOderAnsiKorrektLesen(ByVal DateiPfad As String) As TextdateiRückgabe
+        Dim tmpBuilder As StringBuilder = New StringBuilder()
+        Dim dateiBytes As Byte() = File.ReadAllBytes(DateiPfad)
+        Dim utf8OhneBomDecoder As Decoder = Encoding.UTF8.GetDecoder()
+        utf8OhneBomDecoder.Fallback = New DecoderExceptionFallback()
+        Dim rückgabeStr As TextdateiRückgabe
+        rückgabeStr.Text = ""
+        rückgabeStr.FehlerFlag = False
+        Try
+            Dim decodiertedUtf8Chars As Char() = New Char(utf8OhneBomDecoder.GetCharCount(dateiBytes, 0, dateiBytes.Length, True) - 1) {}
+            utf8OhneBomDecoder.GetChars(dateiBytes, 0, dateiBytes.Length, decodiertedUtf8Chars, 0)
+            Array.ForEach(Of Char)(decodiertedUtf8Chars, Function(c) tmpBuilder.Append(c))
+        Catch fallbackAufAnsiEx As DecoderFallbackException
+            Dim ansiDecoder As Decoder = Encoding.[Default].GetDecoder()
+            ansiDecoder.Fallback = New DecoderExceptionFallback()
+            Try
+                Dim decodedAnsiChars As Char() = New Char(ansiDecoder.GetCharCount(dateiBytes, 0, dateiBytes.Length, True) - 1) {}
+                ansiDecoder.GetChars(dateiBytes, 0, dateiBytes.Length, decodedAnsiChars, 0)
+                Array.ForEach(Of Char)(decodedAnsiChars, Function(c) tmpBuilder.Append(c))
+            Catch keinPassenderDecoderEx As DecoderFallbackException
+                rückgabeStr.FehlerFlag = True
+            End Try
+        End Try
+        '>>> Text in Struktur speichern
+        rückgabeStr.Text = tmpBuilder.ToString
+        '>>> R E T U R N
+        Return rückgabeStr
+    End Function
+    Private Structure TextdateiRückgabe
+        Public Text As String
+        Public FehlerFlag As Boolean
+    End Structure
+#End Region
+
+End Class
